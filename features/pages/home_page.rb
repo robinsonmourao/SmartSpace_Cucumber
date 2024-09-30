3ae7d309243361c7e0e0dc1c9b211d6d7677a40e
robinsonmourao <bob.info.guaratiba@gmail.com>
2024-09-30 07:14:32 -0300

    formatação seguindo rubocop

--- features/pages/home_page.rb
+++ features/pages/home_page.rb
@@ -1,40 +1,39 @@
 class HomePage < SitePrism::Page
-	set_url 'https://smartspace.com.br'
+  set_url 'https://smartspace.com.br'
 
-	element :nome_input, "[id^='rd-text_field-']"
-	element :email_input, "[id^='rd-email_field-']"
-	element :telefone_input, "[id^='rd-phone_field-']"
-	element :estado_select, "[id^='rd-select_field-']"
-	element :empresa_input, "#rd-text_field-lza3jr5t"
-	element :cargo_input, "#rd-text_field-lza3jr5u"
-	element :enviar_button, "[id^='rd-button-']"
+  element :nome_input, "[id^='rd-text_field-']"
+  element :email_input, "[id^='rd-email_field-']"
+  element :telefone_input, "[id^='rd-phone_field-']"
+  element :estado_select, "[id^='rd-select_field-']"
+  element :empresa_input, "#rd-text_field-lza3jr5t"
+  element :cargo_input, "#rd-text_field-lza3jr5u"
+  element :enviar_button, "[id^='rd-button-']"
 
-	def preencher_rd_nome(valor)
-		nome_input.set valor
-	end
+  def preencher_rd_nome(valor)
+    nome_input.set valor
+  end
 
-	def preencher_rd_email(valor)
-		email_input.set valor
-	end
+  def preencher_rd_email(valor)
+    email_input.set valor
+  end
 
-	def preencher_rd_telefone(valor)
-		telefone_input.set valor
-	end
+  def preencher_rd_telefone(valor)
+    telefone_input.set valor
+  end
 
-	def selecionar_rd_estado(valor)
-		estado_select.select(valor)
-	end
+  def selecionar_rd_estado(valor)
+    estado_select.select(valor)
+  end
 
-	def preencher_rd_empresa(valor)
-		empresa_input.set valor
-	end
+  def preencher_rd_empresa(valor)
+    empresa_input.set valor
+  end
 
-	def preencher_rd_cargo(valor)
-		cargo_input.set valor
-	end
+  def preencher_rd_cargo(valor)
+    cargo_input.set valor
+  end
 
-	def clicar_enviar
-		enviar_button.click
-	end
+  def clicar_enviar
+    enviar_button.click
+  end
 end
-	
