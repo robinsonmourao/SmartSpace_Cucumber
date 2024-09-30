4f18733a9adb173402cbfa5facf12f4bf4ce2416
robinsonmourao <bob.info.guaratiba@gmail.com>
2024-09-30 07:16:10 -0300

    sleep para visualização de cada step

--- features/step_definitions/id01-Validar obrigatoriedade do campo 'nome' do formulário 'Transforme o atendimento e comunicação da sua empresa'.rb
+++ features/step_definitions/id01-Validar obrigatoriedade do campo 'nome' do formulário 'Transforme o atendimento e comunicação da sua empresa'.rb
@@ -3,26 +3,32 @@ Dado('que eu tenha acessado o endereço {string}') do |string|
 end
 
 Quando('eu preencher o campo email com valor {string}') do |string|
+  sleep(3)
   find("[id^='rd-email_field-']").set(string)
 end
 
 E('eu preencher o campo celular com valor {string}') do |string|
+  sleep(3)
   find("[id^='rd-phone_field-']").set(string)
 end
 
 E('eu escolher um item para o campo estado com valor {string}') do |string|
+  sleep(3)
   find("[id^='rd-select_field-']").select(string)
 end
 
 E('eu preencher o campo empresa com valor {string}') do |string|
+  sleep(3)
   find("#rd-text_field-lza3jr5t").set(string)
 end
 
 E('eu preencher o campo cargo com valor {string}') do |string|
+  sleep(3)
   find("#rd-text_field-lza3jr5u").set(string)
 end
 
 E('eu clicar no botão Enviar') do
+  sleep(3)
   find("[id^='rd-button-']").click
 end
 
